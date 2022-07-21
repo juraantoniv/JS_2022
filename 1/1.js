ae9809e01893f8dbb30ac27afe20119982211972
juraantoniv <juraantoniv@gmail.com>
2022-07-21 22:17:16 +0300

    Initial commit

---
+++ 1/1.js
@@ -0,0 +1,83 @@
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let l = true;
+// let m = false;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+// console.log(j)
+// console.log(k)
+// console.log(l)
+// console.log(m)
+
+// document.write(a)
+// document.write(b)
+// document.write(c)
+// document.write(d)
+// document.write(e)
+// document.write(f)
+// document.write(g)
+// document.write(h)
+// document.write(i)
+// document.write(j)
+// document.write(k)
+// document.write(l)
+// document.write(m)
+
+// alert(a)
+
+// ------------------------------------
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstName = 'Antoniv'
+// let middleName = 'Yura'
+// let lastName = 'Vasylovych'
+// let person = '';
+//
+// person = firstName+ ' '+ middleName+ ' '+ lastName;
+// console.log(person)
+
+//-------------------------------------
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+//---------------------------------------
+//
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+//
+// let firstName = prompt("Please enter your firstName: ")
+// let middleName = prompt("Please enter your middleName: ")
+// let lastName = prompt("Please enter your lastName: ")
+// let Year = prompt("Please enter your Year: ")
+//
+// console.log(firstName,' ', middleName, ' ' , lastName, ' ',Year)
