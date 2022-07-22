b020258e197e35cd69fb5e0d7ebad3270415708b
juraantoniv <juraantoniv@gmail.com>
2022-07-22 21:35:46 +0300

    ok

--- 1/1.js
+++ 1/1.js
@@ -50,11 +50,11 @@
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
-let firstName = 'Antoniv'
-let middleName = 'Yura'
-let lastName = 'Vasylovych'
-person = `${firstName} ${middleName} ${lastName}`;
-console.log(person)
+// let firstName = 'Antoniv'
+// let middleName = 'Yura'
+// let lastName = 'Vasylovych'
+// person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person)
 
 //-------------------------------------
 
