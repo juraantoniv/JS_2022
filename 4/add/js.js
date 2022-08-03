1604cc6a6710386b73e9468931775f0f1b29841a
juraantoniv <juraantoniv@gmail.com>
2022-08-03 13:35:18 +0300

    ok

--- 4/add/js.js
+++ 4/add/js.js
@@ -698,10 +698,30 @@ let arr = [
     }
 ];
 
+// for (const arrKey in arr) {
+//     let value=arr[arrKey]
+//     document.write(`<div class="wrap">`)
+// for (const arrKey in arr) {
+//     let value=arr[arrKey]
+//     document.write(`<div class="wrap1">`)
+//     document.write(`<div><h2>${value.title}</h2></div>`)
+//     document.write(`<div>${value.userId}- ${value.id}</div>`)
+//     document.write(`<div class="text">${value.body}</div>`)
+//     document.write(`<div class="wrap1">`)
+//
+//     document.write(`</div>`)
+//
+
+
+    document.write(`<div class="wrap"`)
 for (const arrKey in arr) {
     let value=arr[arrKey]
-    document.write(`<div><h2>${value.title}</h2></div>`)
+    document.write(`<div class="wrap1">`)
+    document.write(`<div><h4>${value.title}</h4></div>`)
     document.write(`<div>${value.userId}- ${value.id}</div>`)
     document.write(`<div class="text">${value.body}</div>`)
+    document.write(`<div>`)
+
+    document.write(`</div>"`)
 
 }
